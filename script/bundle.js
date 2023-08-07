1db59d3f302f311ac5eec3f19e3b03ee15d118a9
simplelearn GITHUN <sinha@gmail.com>
2023-08-07 16:05:10 +0530

    change-card

--- script/bundle.js
+++ script/bundle.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Function to hide all tables except the one in the selected section
     function showSelectedTable(selectedTableId) {
         const tables = document.getElementsByClassName("size-colour-tables");
         for (const table of tables) {
@@ -11,7 +10,6 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 
-    // Add click event listeners to each section to show the corresponding table
     const pairOneSection = document.querySelector(".pair-one");
     const pairTwoSection = document.querySelector(".pair-two");
     const pairThreeSection = document.querySelector(".pair-three");
